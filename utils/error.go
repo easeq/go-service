22f067a7bfcabc12fe82b392a07ad65d5c99e1e2
ES <eon@earncef.com>
2021-03-29 12:29:42 +0530

    Added multi-channel based error handler

--- utils/error.go
+++ utils/error.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"database/sql"
 	"errors"
+	"sync"
 
 	"google.golang.org/grpc/codes"
 )
@@ -14,4 +15,42 @@ func GetErrorCode(err error) codes.Code {
 	}
 
 	return codes.Internal
+}
+
+// MergeErrors merges multiple channels of errors.
+func MergeErrors(cs ...<-chan error) <-chan error {
+	var wg sync.WaitGroup
+	out := make(chan error, len(cs))
+
+	// Handle individual error channels
+	output := func(c <-chan error) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Wait and close "out" channel once done
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+// WaitForError waits for results from all error channels
+// Returns on first non-nil error or returns nil after all channels are closed
+func WaitForError(errs ...<-chan error) error {
+	errc := MergeErrors(errs...)
+	for err := range errc {
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
