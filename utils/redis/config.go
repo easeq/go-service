39abca40f71fa61e06cc18f6a7f004d997791878
ES <eon@earncef.com>
2021-05-11 17:20:45 +0530

    Updated redis config defaults

--- utils/redis/config.go
+++ utils/redis/config.go
@@ -7,27 +7,27 @@ import (
 )
 
 type Config struct {
-	Network  string `env:"REDIS_NETWORK"`
-	Addr     string `env:"REDIS_ADDRESS"`
-	Username string `env:"REDIS_USERNAME"`
-	Password string `env:"REDIS_PASSWORD"`
+	Network  string `env:"REDIS_NETWORK,default=tcp"`
+	Addr     string `env:"REDIS_ADDRESS,default=localhost:6379"`
+	Username string `env:"REDIS_USERNAME,default="`
+	Password string `env:"REDIS_PASSWORD,default="`
 
-	DB int `env:"REDIS_DB"`
+	DB int `env:"REDIS_DB,default="`
 
-	MaxRetries      int           `env:"REDIS_MAX_RETRIES"`
-	MinRetryBackoff time.Duration `env:"REDIS_MIN_RETRY_BACKOFF"`
-	MaxRetryBackoff time.Duration `env:"REDIS_MAX_RETRY_BACKOFF"`
+	MaxRetries      int           `env:"REDIS_MAX_RETRIES,default=3"`
+	MinRetryBackoff time.Duration `env:"REDIS_MIN_RETRY_BACKOFF,default=8ms"`
+	MaxRetryBackoff time.Duration `env:"REDIS_MAX_RETRY_BACKOFF,default=512ms"`
 
-	DialTimeout  time.Duration `env:"REDIS_DIAL_TIMEOUT"`
-	ReadTimeout  time.Duration `env:"REDIS_READ_TIMEOUT"`
-	WriteTimeout time.Duration `env:"REDIS_WRITE_TIMEOUT"`
+	DialTimeout  time.Duration `env:"REDIS_DIAL_TIMEOUT,default=5s"`
+	ReadTimeout  time.Duration `env:"REDIS_READ_TIMEOUT,default=3s"`
+	WriteTimeout time.Duration `env:"REDIS_WRITE_TIMEOUT,default="`
 
-	PoolSize           int           `env:"REDIS_POOL_SIZE"`
-	MinIdleConns       int           `env:"REDIS_MIN_IDLE_CONNS"`
-	MaxConnAge         time.Duration `env:"REDIS_MAX_CONN_AGE"`
-	PoolTimeout        time.Duration `env:"REDIS_POOL_TIMEOUT"`
-	IdleTimeout        time.Duration `env:"REDIS_IDLE_TIMEOUT"`
-	IdleCheckFrequency time.Duration `env:"REDIS_IDLE_CHEKC_FREQUENCY"`
+	PoolSize           int           `env:"REDIS_POOL_SIZE,default=10"`
+	MinIdleConns       int           `env:"REDIS_MIN_IDLE_CONNS,default="`
+	MaxConnAge         time.Duration `env:"REDIS_MAX_CONN_AGE,default="`
+	PoolTimeout        time.Duration `env:"REDIS_POOL_TIMEOUT,default="`
+	IdleTimeout        time.Duration `env:"REDIS_IDLE_TIMEOUT,default="`
+	IdleCheckFrequency time.Duration `env:"REDIS_IDLE_CHEKC_FREQUENCY,default=60s"`
 }
 
 // UnmarshalEnv env.EnvSet to Config
