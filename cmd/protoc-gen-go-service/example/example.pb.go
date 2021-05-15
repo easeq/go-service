dc6bdb70af7a50bdaf83d9993613f5dc98ec81ad
ES <eon@earncef.com>
2021-05-15 16:09:35 +0530

    Fixed incorrect package name in generated files

--- cmd/protoc-gen-go-service/example/example.pb.go
+++ cmd/protoc-gen-go-service/example/example.pb.go
@@ -7,8 +7,8 @@
 package example
 
 import (
-	_ "../options"
 	context "context"
+	_ "github.com/easeq/go-service/cmd/protoc-gen-go-service/options"
 	empty "github.com/golang/protobuf/ptypes/empty"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
